7d023e293e090902ec30f6ac0a8aa262625033f3
rafaelacorrea <rafaela.f.c.correa@gmail.com>
2023-03-04 21:03:34 -0300

    feat: :sparkles: adding tetromino rotate

---
+++ lib/tetris/tetromino.ex
@@ -0,0 +1,40 @@
+defmodule Tetris.Tetromino do
+  defstruct shape: :l, rotation: 0, location: {5, 1}
+  alias Tetris.Point
+
+  def new(option \\ []) do
+    __struct__(option)
+  end
+
+  def new_random() do
+    new(shape: random_shape())
+  end
+
+  def right(tetro) do
+    %{ tetro | location: Point.right(tetro.location)}
+  end
+
+  def left(tetro) do
+    %{ tetro | location: Point.left(tetro.location)}
+  end
+
+  def down(tetro) do
+    %{ tetro | location: Point.down(tetro.location)}
+  end
+
+  def rotate(tetro) do
+    %{ tetro | rotation: rotate_degrees(tetro.rotation)}
+  end
+  defp random_shape do
+    ~w(i j l o s t z)a
+    |> Enum.random
+  end
+
+  def rotate_degrees(270) do
+    0
+  end
+
+  def rotate_degrees(n) do
+    n + 90
+  end
+end
